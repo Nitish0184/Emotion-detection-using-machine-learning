910ef7eba95ddd5b9ea8098af3ec09ccf752ff53
Nitish0184 <nitishkumaryadav8401@gmail.com>
2024-08-15 08:53:09 +0530

    experimetnt-1 added

---
+++ notebooks/mlflow-dagshub.py
@@ -0,0 +1,11 @@
+import mlflow
+
+mlflow.set_tracking_uri('https://dagshub.com/Nitish0184/mlops-project.mlflow')
+
+import dagshub
+dagshub.init(repo_owner='Nitish0184', repo_name='mlops-project', mlflow=True)
+
+
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
