8e4806dabe1693afd0daff125dd5b99bfe0ce3a9
Nitish0184 <nitishkumaryadav8401@gmail.com>
2024-08-15 10:24:34 +0530

    added s3 remote

--- notebooks/mlflow-dagshub.py
+++ notebooks/mlflow-dagshub.py
@@ -1,10 +1,10 @@
+
 import mlflow
 
 mlflow.set_tracking_uri('https://dagshub.com/Nitish0184/mlops-project.mlflow')
 
 import dagshub
 dagshub.init(repo_owner='Nitish0184', repo_name='mlops-project', mlflow=True)
-
 
 with mlflow.start_run():
   mlflow.log_param('parameter name', 'value')
