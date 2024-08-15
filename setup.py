8318f1d7daf87b6342e9d48d4e93d4ba1086a11f
Nitish0184 <nitishkumaryadav8401@gmail.com>
2024-08-15 08:08:42 +0530

    initilised git hub

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this is mini project of emotion detection in humans',
+    author='Nitish Yadav',
+    license='',
+)
